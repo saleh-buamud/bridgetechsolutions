fe3d2e44e28ef7451b63daeaedb7c8ef22c52618
Saleh buamud <bbuamud@gmail.com>
2024-11-20 12:51:26 +1400

    Update project

--- main.js
+++ main.js
@@ -1,41 +1,38 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // عمل "active" للروابط في النافبار
-    var navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+// document.addEventListener('DOMContentLoaded', function () {
+//     // عمل "active" للروابط في النافبار
+//     var navLinks = document.querySelectorAll('.navbar-nav .nav-link');
     
-    navLinks.forEach(function (link) {
-        link.addEventListener('click', function () {
-            navLinks.forEach(function (link) {
-                link.classList.remove('active');
-            });
-            this.classList.add('active');
-        });
-    });
+//     navLinks.forEach(function (link) {
+//         link.addEventListener('click', function () {
+//             navLinks.forEach(function (link) {
+//                 link.classList.remove('active');
+//             });
+//             this.classList.add('active'); document.addEventListener("DOMContentLoaded", function () {
+                let currentUrl = window.location.href;
+                let navLinks = document.querySelectorAll(".navbar .nav-link, .footer .nav-link");
 
-    // عمل "active" للروابط في الفوتر
-    var footerLinks = document.querySelectorAll('.footer .nav-link');
+                navLinks.forEach(function (link) {
+                    if (currentUrl.includes(link.href)) {
+                        link.classList.add("active");
+                    } else {
+                        link.classList.remove("active");
+                    }
+                });
+        
+//         });
+//     });
+
+//     // عمل "active" للروابط في الفوتر
+//     var footerLinks = document.querySelectorAll('.footer .nav-link');
     
-    footerLinks.forEach(function (link) {
-        link.addEventListener('click', function () {
-            footerLinks.forEach(function (link) {
-                link.classList.remove('active');
-            });
-            this.classList.add('active');
-        });
-    });
-});
+//     footerLinks.forEach(function (link) {
+//         link.addEventListener('click', function () {
+//             footerLinks.forEach(function (link) {
+//                 link.classList.remove('active');
+//             });
+//             this.classList.add('active');
+//         });
+//     });
+// });
 
-  document.addEventListener('DOMContentLoaded', function() {
-        var toggler = document.querySelector('.navbar-toggler');
-        var barsIcon = toggler.querySelector('.fa-bars');
-        var timesIcon = toggler.querySelector('.fa-times');
-
-        toggler.addEventListener('click', function() {
-            if (toggler.classList.contains('collapsed')) {
-                barsIcon.classList.remove('d-none');
-                timesIcon.classList.add('d-none');
-            } else {
-                barsIcon.classList.add('d-none');
-                timesIcon.classList.remove('d-none');
-            }
-        });
-    });
+ 
